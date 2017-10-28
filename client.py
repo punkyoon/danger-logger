2b09a2a529c6ee9ebe72a12b3b8a33b317ed58ed
punkyoon <cryptography@konkuk.ac.kr>
2017-10-28 19:19:09 +0000

    using REUSEADDR and checking passcode, etc..

--- client.py
+++ client.py
@@ -4,18 +4,29 @@ import socket
 _SERVER_IP = '127.0.0.1'
 _PORT = 8080
 
-_BUFSIZ = io.DEFAULT_BUFFER_SIZE
+#_BUFSIZ = io.DEFAULT_BUFFER_SIZE
+_BUFSIZ = 64
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((_SERVER_IP, _PORT))
 
+_passcode = input('[INP] PASSCODE: ')
+
+client_socket.send(_passcode.encode())
+is_passed = client_socket.recv(_BUFSIZ).decode()
+
+if is_passed != 'True':
+    print('[MSG] PASSCODE does not match')
+    exit(1)
+
 while True:
-    cmd = input()
-    client_socket.send(cmd)
+    cmd = input('[INP] command: ')
+    client_socket.send(cmd.encode())
     
-    response_data = client_socket.recv(_BUFSIZ)
+    response_data = client_socket.recv(_BUFSIZ).decode()
     if response_data == 'quit':
         break
     print('[MSG] %s' % response_data)    # Temporary code line
 
+print('[MSG] Closing client socket ...')
 client_socket.close()
--- server.py
+++ server.py
@@ -9,28 +9,38 @@ _PORT = 8080
 _BUFSIZ = 64
 #_BUFSIZ = io.DEFAULT_BUFFER_SIZE
 
-_PASSCODE = os.environ['PASSCODE']
+#_PASSCODE = os.environ['PASSCODE']
+_PASSCODE = 'test'
 
+print('[MSG] Creating server socket ...')
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind((_IP, _PORT))
 server_socket.listen(1)
+print('[MSG] Server socket is created')
 
 connection, addr = server_socket.accept()
-print('[MSG] Client Connection - %s' % addr)
+print('[MSG] Client Connection - %s' % str(addr[0]))
 
-pass_code = connection.recv(_BUFSIZ)
+pass_code = connection.recv(_BUFSIZ).decode()
 if _PASSCODE != pass_code:
     print('[ERR] PASSCODE from client does not match')
+    connection.send('False'.encode())
     exit(1)
 
+connection.send('True'.encode())
 print('[MSG] Client authentication completed')
 
 while True:
-    cmd = connection.recv(_BUFSIZ)
-    if cmd == 'quit':
-        break
+    cmd = connection.recv(_BUFSIZ).decode()
 
     print('[CMD] %s' % cmd)
-    connection.send(cmd)    # Temporary code line
+    connection.send(cmd.encode())    # Temporary code line
 
-connection.close()
+    if cmd == 'quit':
+        connection.close()
+        break
+
+print('[MSG] Closing server socket ...')
+server_socket.shutdown(1)
+server_socket.close()
