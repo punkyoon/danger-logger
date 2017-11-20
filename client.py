c5a2a85832245d3bc9ff4fa0990d308b754fe9a3
punkyoon <cryptography@konkuk.ac.kr>
2017-11-20 04:31:55 +0000

    check passcode and control docker web server

--- client.py
+++ client.py
@@ -1,9 +1,24 @@
 import socket
+import hashlib
 
 
 if __name__ == '__main__':
+    _IS_PASSED = False
+
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(('localhost', 8080))
+
+    cmd = input('[AUTH] passcode: ')
+    pass_hash = hashlib.sha256(cmd.encode()).hexdigest()
+    sock.send(pass_hash.encode())
+
+    is_passed = sock.recv(1024).decode()
+    if is_passed == 'False':
+        print('[MSG]Invalid passcode')
+        sock.close()
+        exit(0)
+
+    print('[MSG]Valid passcode')
     while True:
         cmd = input('[INP] command: ')
         #sock.sendall(cmd.encode())
@@ -12,6 +27,7 @@ if __name__ == '__main__':
         result = sock.recv(1024).decode()
         print(result)
         if result == 'quit':
+            print('[MSG]Shutdown..')
             break
 
     sock.close()
--- server.py
+++ server.py
@@ -1,4 +1,6 @@
+import os
 import socket
+import hashlib
 import logging
 import subprocess
 import multiprocessing
@@ -6,36 +8,78 @@ import multiprocessing
 from multiprocessing import Process
 
 
-_SERVER_PATH = '/home/punk/study-hard'
-_PASS_CODE = 'testestest'
+_SERVER_PATH = os.path.join(
+    '/home/punk/study-hard',
+    'docker-compose.yaml'
+)
+_PASS_CODE = 'testtesttest'
 _IS_ALIVE = False
 
 
 def docker_start():
-    subprocess.run(['docker-compose', 'up', '-d'])
+    subprocess.run(['docker-compose', '-f', _SERVER_PATH, 'up', '-d'])
 
 
 def docker_down():
-    subprocess.run(['docker-compose', 'down'])
+    subprocess.run(['docker-compose', '-f', _SERVER_PATH, 'down'])
+
+
+def get_log():
+    if is_docker_alive():
+        output_result = subprocess.check_output([
+            'docker-compose', '-f',
+            _SERVER_PATH, 'logs', '--no-color'
+        ])
+
+        with open('logs.txt', 'wb') as f:
+            f.write(output_result)
+
+        return True
+    else:
+        return False
 
 
 def start_web_server():
-    proc = Process(target=docker_start)
+    global _IS_ALIVE
     _IS_ALIVE = True
+    Process(target=docker_start)
 
 
 def stop_web_server():
-    proc = Process(target=docker_start)
-    _IS_ALIVE = False
+    global _IS_ALIVE
+    _IS_ALIVE = True
+    Process(target=docker_start)
 
 
-def is_alive_docker():
+def is_docker_alive():
+    global _IS_ALIVE
     return _IS_ALIVE
+
+
+def check_passcode(hash_value):
+    global _PASS_CODE
+    origin_value = hashlib.sha256(_PASS_CODE.encode()).hexdigest()
+    if origin_value == hash_value:
+        return True
+    else:
+        return False
 
 
 def handle(connection, addr):
     logging.basicConfig(level=logging.DEBUG)
     logger = logging.getLogger('process-%r' % (addr,))
+
+    pass_code = connection.recv(1024).decode()
+    if not check_passcode(pass_code):
+        connection.send('False'.encode())
+        logger.debug('Invalid passcode - connection refused')
+        connection.close()
+        exit(0)
+
+    msg = 'Valid passcode - connected successfully'
+    connection.send(msg.encode())
+    logger.debug(msg)
+
     try:
         logger.debug('Connected %r at %r' % (connection, addr))
         while True:
@@ -44,9 +88,35 @@ def handle(connection, addr):
                 logger.debug('socket closed remotely')
                 break
             logger.debug('Received data %r' % data)
+
+            msg = ''
+            if data == 'start':
+                if is_docker_alive():
+                    msg = 'Web server on docker is started'
+                    start_web_server()
+                else:
+                    msg = 'Web server is already running on docker'
+
+            elif data == 'stop':
+                if is_docker_alive():
+                    msg = 'We server on docker is stopped'
+                    stop_web_server()
+                else:
+                    msg = 'Web server on docker is not running'
+
+            elif data == 'status':
+                msg = 'Web server on docker status: %s' % (is_docker_alive(),)
+
+            elif data == 'quit':
+                msg = 'quit'
+
+            else:
+                msg = 'Invalid command'
+
             # connection.sendall(data.encode())
-            connection.send(data.encode())
-            logger.debug('Sent data %r' % data)
+            logger.debug(msg)
+            connection.send(msg.encode())
+            logger.debug('Sent data %r' % msg)
     except:
         logger.exception('Problem handling request')
     finally:
